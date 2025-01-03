b8bd8ce8bfe68e4b3a85b3d00d149f2ba2a29e27
Niklas Nordkamp <nnord@d-velop.de>
2025-01-03 16:56:34 +0100

    Initial Commit

---
+++ workoutexport/workoutexportApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutexportApp.swift
+//  workoutexport
+//
+//  Created by Nordkamp, Niklas on 03.01.25.
+//
+
+import SwiftUI
+
+@main
+struct workoutexportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
